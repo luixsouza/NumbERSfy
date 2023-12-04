5e1338f757695f3b1a4caa0f711cf43a303c39fd
luixsouza <111254736+luixsouza@users.noreply.github.com>
2023-12-03 23:46:36 -0300

    Add files via upload

---
+++ NumbERSfy/src/main/java/com/ifg/NumbERSfy/MusicController.java
@@ -0,0 +1,25 @@
+package com.ifg.NumbERSfy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MusicController {
+
+    @GetMapping("/getMusicInfo")
+    public String getMusicInfo(Model model) {
+    
+        MusicInfo musicInfo = new MusicInfo(
+            "/static/images/cranberries2.jpg",
+            "Nome da Música",
+            "Nome do Artista"
+        );
+
+    
+        model.addAttribute("musicInfo", musicInfo);
+        
+        return "lista";
+    }
+}
+
---
+++ NumbERSfy/src/main/java/com/ifg/NumbERSfy/NumbERSfyApplication.java
@@ -0,0 +1,13 @@
+package com.ifg.NumbERSfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbERSfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbERSfyApplication.class, args);
+	}
+
+}
